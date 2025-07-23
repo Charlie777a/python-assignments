bf004618af918b4dff513cdd0ad1f52f406d0355
Ankit777 <167916996+Charlie777a@users.noreply.github.com>
2025-07-23 18:32:28 +0530

    Delete TTDassignment1-Task1.py

--- TTDassignment1-Task1.py
+++
@@ -1,7 +0,0 @@
-a=int(input("enter first number: "))
-b=int(input("enter second number: "))
-
-print("\n\nAddition: ",a+b)
-print("Subtraction: ",a-b)
-print("Multiplication: ",a*b)
-print("Division: ",a/b)
