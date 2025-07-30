3a69c8d42a0e28e14f0e6d6e30f6da07b583853c
Ankit777 <167916996+Charlie777a@users.noreply.github.com>
2025-07-30 18:45:38 +0530

    Add files via upload
    Fourth assignment for python:
    
    Task 1:  Reading the content of a file and finding out error if file is not present using get except method.
    output will be:              (can be different because txt file is not shared) 
     
    Reading file content:
    line 1:This is a sample text file.
    line 2:It contain multiple lines.
    
    
    Task 2:  writing in a file after that appending it with extra data and at last reading it.
    output will be  :          (can be different because txt file is not shared) 
    
    Entre text to write in the file: Hello, Python!
    Data successfully written to output.txt.
    
    Enter additional text to append: Learning file handling in python.
    Data successfully appended
    
    
    final content of 'output.txt' file:
    Hello, Python!
    Learning file handling in python.

---
+++ assignment_4/TTDassignment4-Task1.py
@@ -0,0 +1,11 @@
+try:
+  file=open('sample.txt','r')
+  print("Reading file content:")
+  line1=file.readline()
+  line2=file.readline()
+  print(f"line 1:{line1.strip()}")
+  print(f"line 2:{line2.strip()}")
+  file.close()
+
+except FileNotFoundError:
+  print("The file'sample.txt' not found ")
